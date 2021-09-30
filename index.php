38cb7e6524ebeba5d96b8bf062b38c16556ff693
nindyavinalia <89982647+nindyavinalia@users.noreply.github.com>
2021-09-30 22:38:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Massage and Spa Beauty House</h2>
+	<p>Welcome sistur! enjoy for treatment <3</p>
+</div>
+
+<?=template_footer()?>
+
